9825cc8a73531c072d72fb66f1be8e8d4378d07e
Henry Jonathan <henryjonathanchandra12@gmail.com>
2025-09-02 17:47:32 +0700

    setup frontend with QwikJS + Tailwind CSS

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,27 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+    <div class="text-center text-2xl mt-5">
+      <h1>Hi 👋</h1>
+      <div>
+        Can't wait to see what you build with qwik!
+        <br />
+        Happy coding.
+      </div>
+    </div>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
